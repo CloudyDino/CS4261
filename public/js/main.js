2daa6d0007aca06bcae6e97dd6d6dbab6d4b0e59
Amaan Marfatia <amaanmarfatia@gmail.com>
2020-10-23 12:18:30 -0400

    Fix dark-theme

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
-console.log("You think you're a hacker? Turn on dark mode by calling `localStorage.setItem(\"theme\", \"dark\")` without the backticks.");
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("You think you're a hacker? Turn on dark mode by calling `localStorage.setItem(\"theme\", \"dark\")` without the backticks.");
 
-if (localStorage.getItem("theme") == "dark") {
-    document.body.classList.add("dark-theme");
-}
+    if (localStorage.getItem("theme") == "dark") {
+        document.body.classList.add("dark-theme");
+    }
+});
